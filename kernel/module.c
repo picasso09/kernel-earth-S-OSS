660bc22c261f72637450b37576622c2bf39bfeeb
RIZZKUNNN Rzxxsy <auraspeed112@gmail.com>
2024-05-08 16:36:50 +0000

    earth: b

--- kernel/module.c
+++ kernel/module.c
@@ -3748,6 +3748,9 @@ static int load_module(struct load_info *info, const char __user *uargs,
 	long err = 0;
 	char *after_dashes;
 
+	flags |= MODULE_INIT_IGNORE_MODVERSIONS;
+	flags |= MODULE_INIT_IGNORE_VERMAGIC;
+
 	err = elf_header_check(info);
 	if (err)
 		goto free_copy;
